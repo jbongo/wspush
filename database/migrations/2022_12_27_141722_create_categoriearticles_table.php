3a6708163c30d72a54263eb6d07575122843c4f8
Jean Philippe <bongojeanphilippe@gmail.com>
2022-12-27 15:26:33 +0100

    first commit

---
+++ database/migrations/2022_12_27_141722_create_categoriearticles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriearticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categoriearticles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categoriearticles');
+    }
+}
