8c27856ac1dbeed7a58e9dabb058f724b640593e
Jean Philippe <bongojeanphilippe@gmail.com>
2023-03-04 03:34:22 +0100

    changer background menu

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -97,6 +97,7 @@ class ArticleController extends Controller
                 'password' => $siteinterne->password,
             ]);
     
+            
             $token = $response->json()['token'] ;
             
             $curl = curl_init();
@@ -130,7 +131,7 @@ class ArticleController extends Controller
             } else {
     
                 $fileResponse = json_decode($response,true);
-    
+                dd($token);
                 $resp = Http::withToken($token)
                 ->post("$domaine/wp-json/wp/v2/posts",
     
